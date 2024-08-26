db3e26ab3404d0384951671b912dd90e6da1cf0d
Giovanna Franco <rm553701@fiap.com.br>
2024-08-26 19:15:05 -0300

    add section 3 + style

--- GetDrink/src/Componentes/Section3.jsx
+++ GetDrink/src/Componentes/Section3.jsx
@@ -1,9 +1,45 @@
 function Section3() {
     return (
         <>
-            <main>
-                <div>
-                    getdrink
+            <main className="Section3">
+                <div className="section-three">
+                    <h1>TRANSFORME SEU EVENTO</h1>
+                    <div className="content">
+                        <div>
+                            <ul> Ativação da Marca
+                                <li>Eventos Esportivos</li>
+                                <li>Corporativo</li>
+                                <li>Lançamentos de Produtos</li>
+                                <li>Blitz e Sampling</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul> Eventos de Grande Porte
+                                <li>Festivais</li>
+                                <li>Salas de Concerto</li>
+                                <li>Rodeios</li>
+                                <li>Estádios/Arenas</li>
+                                <li>Eventos públicos de rua</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul> Eventos de Médio Porte
+                                <li>Área Vip</li>
+                                <li>Experiências</li>
+                                <li>Graduações</li>
+                                <li>Resorts e Hotéis</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul> Eventos de Pequeno Porte
+                                <li>Casamentos</li>
+                                <li>Eventos Sociais</li>
+                                <li>Aniversários</li>
+                                <li>Bares</li>
+                            </ul>
+                        </div>
+
+                    </div>
                 </div>
             </main>
         </>
