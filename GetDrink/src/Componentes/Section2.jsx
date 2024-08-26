c1a5879f39030e0d32df5200b78acffad2b768ca
Giovanna Franco <rm553701@fiap.com.br>
2024-08-26 18:41:39 -0300

    add section sobre + style

--- GetDrink/src/Componentes/Section2.jsx
+++ GetDrink/src/Componentes/Section2.jsx
@@ -1,9 +1,66 @@
+import cups from '../../public/svg/cups.svg'
+import maps from '../../public/svg/maps.svg'
+import moeda from '../../public/svg/moeda.svg'
+import like from '../../public/svg/like.svg'
+import patrocinio from '../../public/svg/patrocinio.svg'
+import publicomarca from '../../public/svg/publico-marca.svg'
 function Section2() {
     return (
         <>
-            <div>
-                <h1>GetDrink</h1>
-            </div>
+            <main className="sobre">
+                <section>
+                    <h1>A EVOLUÇÃO NO MERCADO DE EVENTOS</h1>
+                    <div className="sobre-content">
+                        <div>
+                            <div className='content'>
+                                <img src={cups} alt="" />
+                                <p>Venda com variação de tamanho</p>
+                            </div>
+                            <div className='content'>
+                                <img src={moeda} alt="" />
+                                <p>Mesmo funcionário realiza vendas e atendimento</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className='content'>
+                                <img src={maps} alt="" />
+                                <p>Venda e atendimento em qualquer local do evento</p>
+                            </div>
+                            <div className='content'>
+                                <img src={like} alt="" />
+                                <p>Redução de filas</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className='content'>
+                                <img src={publicomarca} alt="" />
+                                <p>Interação público-marca</p>
+                            </div>
+                            <div className='content'>
+                                <img src={patrocinio} alt="" />
+                                <p>Promoção de marcas e patrocínios</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='sobre-footer'>
+                        <div className='content'>
+                            <h1>Experiência</h1>
+                            <p>Melhore a experiência do seu cliente</p>
+                        </div>
+                        <div className='content'>
+                            <h1>Visibilidade</h1>
+                            <p>Forneça conteúdo personalizado em alta definição</p>
+                        </div>
+                        <div className='content'>
+                            <h1>Versatilidade</h1>
+                            <p>Venda e sirva bebidas de forma simples em qualquer local</p>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
