922afad2edbccab7ca69d2c3c8f8af7a354904e9
Giovanna Franco <rm553701@fiap.com.br>
2024-08-26 20:12:02 -0300

    add section 4 + style

--- GetDrink/src/Componentes/Section4.jsx
+++ GetDrink/src/Componentes/Section4.jsx
@@ -1,9 +1,47 @@
-function Section4(){
-    return(
+import logo from '../../public/svg/getdrink.svg'
+import linkedin from '../../public/svg/linkedin.svg'
+import instagram from '../../public/svg/instagram.svg'
+import tiktok from '../../public/svg/tik-tok.svg'
+function Section4() {
+    return (
         <>
-        <main>
-            oi
-        </main>
+            <main className="Section4">
+                <section className="formulário-section">
+                    <div>
+                        <h1>Solicite um orçamento sem compromisso</h1>
+                        <p>Preencha seus dados e transforme seu evento com a Get Drink</p>
+                        <form action="">
+
+                            <input type="text" placeholder="Nome" id="name" className="form" />
+                            <input type="tel" placeholder="Telefone/Whatsapp" id="tel" className="form" />
+
+
+                            <input type="email" placeholder="Email" />
+                            <input type="text" placeholder="Tipo de Evento" />
+                            <input type="textarea" placeholder="Detalhes do Evento" />
+                            <div>
+                                <button type="submit">Solicitar Orçamento</button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </main>
+            <main className="Contato">
+                <section className="contato">
+                    <h1>CONTATO</h1>
+
+                    <div className='content'>
+                        <div className='midiasocial'>
+                            <img src={linkedin} alt="" />
+                            <img src={instagram} alt="" />
+                            <img src={tiktok} alt="" />
+                        </div>
+                        <p>@get.drink</p>
+                        <p>comercial@get.drink.com</p>
+                    </div>
+                    
+                </section>
+            </main>
         </>
     )
 }
